91feb51b98725b7de637f35b57cb0a80e7e1168b
Chloe Urisohn <chloeurisohn@gmail.com>
2020-12-23 19:09:58 +0000

    first commit

---
+++ footer.php
@@ -0,0 +1,53 @@
+<footer class="footer mt-auto py-3 bg-dark">
+  <div class="container">
+<div class="row mb-2">
+<h1 class="blog-header-logo">
+  <a href="<?php echo site_url() ?>" class="link-light"><strong><?php bloginfo('name'); ?></strong></a>
+</h1>
+<nav>
+  <ul>
+    <li><a href="<?php echo site_url('/about-us/') ?>">About Us</a></li>
+    <li><a href="#">Programs</a></li>
+    <li><a href="#">Events</a></li>
+  </ul>
+</nav>
+<nav>
+  <ul>
+    <li><a href="#">Legal</a></li>
+    <li><a href="#">Privacy</a></li>
+    <li><a href="#">Careers</a></li>
+  </ul>
+</nav>
+
+<div>
+<h3>Connect With Us</h3>
+<div class="row mb-2">
+<nav>
+<ul>
+  <li class="col">
+    <a href="#" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+  </li>
+  <li class="col">
+    <a href="#" class="social-color-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+  </li>
+  <li class="col">
+    <a href="#" class="social-color-youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+  </li>
+  <li class="col">
+    <a href="#" class="social-color-linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+  </li>
+  <li class="col">
+    <a href="#" class="social-color-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+  </li>
+</ul>
+</nav>
+</div>
+</div>
+
+</div>
+</div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+function loadFiles() {
+    wp_enqueue_style('bootstrap', '//stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css');
+    wp_enqueue_style('roboto-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+    wp_enqueue_style('dmserif-google-fonts', '//fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&display=swap');
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('main', get_stylesheet_directory_uri().'/css/main.css', array(), false, 'all');
+}
+add_action('wp_enqueue_scripts', 'loadFiles');
+function addFeatures() {
+    add_theme_support('title-tag');
+}
+add_action('after_setup_theme', 'addFeatures');
+add_theme_support( 'post-thumbnails' );
+add_theme_support( 'menus' );
+?>
+
