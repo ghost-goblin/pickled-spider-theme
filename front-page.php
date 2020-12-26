92e048d203f3349731c913a8491da1a5c0000632
Chloe Urisohn <chloeurisohn@gmail.com>
2020-12-26 13:59:53 +0000

    Add Img

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
-<img class="front-page-img" src=<?php echo get_template_directory_uri()."/images/drawing.png"?> alt="#">
 <section class="page-wrap">
+<img class="front-page-img" src=<?php echo get_template_directory_uri()."/images/drawing.svg"?> alt="#">
 <div class="container">
     <h1><?php the_title(); ?></h1>
     <?php get_template_part('includes/section', 'content'); ?>
